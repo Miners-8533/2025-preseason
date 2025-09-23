d3be14c8ca1aa1016174316c3e6ecd91980ecc3f
rise <mkblaser@users.noreply.github.com>
2025-09-23 19:21:49 -0400

    move to FTC SDK for flywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
@@ -1,31 +1,38 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
+@Config
 public class Flywheel {
-    private MotorWithEncoderAndVelocityController fly_motor;
+    //private MotorWithEncoderAndVelocityController fly_motor;
+    private DcMotorEx fly_motor;
+    public static double targetVelocity = 0.0;
+    public static double kP = 1.0;
+    public static double kD = 1.0;
+    public static double kF = 0.0;
     public Flywheel(HardwareMap hardwareMap) {
-        MotorWithEncoderAndVelocityController.Config config = new MotorWithEncoderAndVelocityController.Config(
-                "fly_motor",
-                new PIDFCoefficients(1.0, 1.0, 0.0, 0.0),
-                0.0,
-                DcMotorSimple.Direction.FORWARD,
-                100,
-                false
-        );
-        fly_motor = new MotorWithEncoderAndVelocityController(hardwareMap, config);
+        fly_motor = hardwareMap.get(DcMotorEx.class, "fly_motor");
+        fly_motor.setVelocityPIDFCoefficients(kP, 0.0, kD, kF);
+        fly_motor.setVelocity(0.0);
     }
 
     public void update() {
-        fly_motor.update();
+        fly_motor.setVelocityPIDFCoefficients(kP, 0.0, kD, kF);
+        fly_motor.setVelocity(targetVelocity);
     }
 
     public void log(Telemetry tele) {
-        fly_motor.log(tele);
+        tele.addData("Fly motor encoder ticks per second",  fly_motor.getVelocity());
+        tele.addData("Fly motor current (A)",               fly_motor.getCurrent(CurrentUnit.AMPS));
+        tele.addData("Fly motor power (+/-%FS)",            fly_motor.getPower());
     }
 
 }
