b382f231a271861e1eab6e98f386b8dec2ed8ed5
rise <mkblaser@users.noreply.github.com>
2025-11-12 23:20:23 -0500

    Juggling TestOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestingTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestingTeleOp.java
@@ -6,10 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Launcher;
+import org.firstinspires.ftc.teamcode.subsystems.Limelight;
 import org.firstinspires.ftc.teamcode.subsystems.PoseStorage;
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
-@Disabled
+import org.firstinspires.ftc.teamcode.subsystems.SubSystemConfigs;
+
+//@Disabled
 @TeleOp(name="TeleOp - Testing", group="Testing")
 public class TestingTeleOp extends LinearOpMode {
     @Override
@@ -19,12 +23,26 @@ public class TestingTeleOp extends LinearOpMode {
         //Robot robot = new Robot(hardwareMap, gamepad1, gamepad2, PoseStorage.poseStorage);
 
         Launcher launcher = new Launcher(hardwareMap);
+        Intake intake = new Intake(hardwareMap);
+        Limelight limelight = new Limelight(hardwareMap);
 
         waitForStart();
-
+//        intake.intakePower = SubSystemConfigs.INTAKE_RUN;
+//        launcher.stopTarget = SubSystemConfigs.STOP_OPEN;
+        launcher.autoBallCount = 0;
+        //launcher.isSetForSpin = true;
         while (opModeIsActive()) {
-            launcher.testingPower();
+//            if(launcher.isVelocityGoodAuton()) {
+//                intake.transportPower = SubSystemConfigs.TRANSPORT_INTAKE;
+//            } else {
+//                intake.transportPower = SubSystemConfigs.TRANSPORT_STOP;
+//            }
+//            launcher.update();
+//            intake.update();
+            limelight.update();
             launcher.log(telemetry);
+            intake.log(telemetry);
+            limelight.log(telemetry);
             telemetry.update();
             //robot.updateTeleOp(telemetry);
         }
